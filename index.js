54ed265ccdec76cdb813a68fc91183376fe63610
Van Bui <tichondrius.btv@gmail.com>
2019-08-16 16:36:28 +0700

    ADd option for prerender

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const prerender = require('prerender');
-const server = prerender();
+const server = prerender({
+  chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars']
+});
 server.start();
