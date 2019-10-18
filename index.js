ac2abb380f23f323e64990fb620b52caba5e13dd
Van Bui <tichondrius.btv@gmail.com>
2019-10-18 08:18:13 +0000

    Add option

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const prerender = require('prerender');
 const server = prerender({
+  
+  followRedirects: true,
   chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars']
 });
 server.start();
